54e3431c3a3f4621b11285740ba33a1a381911b6
cioran0 <michaelmurdoch0@gmail.com>
2025-05-22 02:40:04 -0400

    Update osdPreview.py

--- osdPreview.py
+++ osdPreview.py
@@ -51,7 +51,7 @@ def open_dzi_file():
         
         # Launch WebView, loading the HTML file and setting the base URL to the DZI's directory
         webview.create_window(
-            'DZI Viewer',
+            'DZI Viewer by cioran0',
             url=f"file://{html_file_path.replace('\\', '/')}", # Load the HTML file
             width=1000,
             height=800,
